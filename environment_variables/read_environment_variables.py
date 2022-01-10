c78ec2f43da3bca53be4ee5eb0c77c6f0fc32ed9
Paul Vincent Craven <paul@cravenfamily.com>
2022-01-10 09:07:08 -0600

    Update

--- environment_variables/read_environment_variables.py
+++ environment_variables/read_environment_variables.py
@@ -4,6 +4,11 @@ variables.
 
 For better security, your password would be encrypted and you'd
 need to decrypt it before use. That's a separate lesson.
+
+Look up how to:
+1.) Set environment variables for your system.
+2.) Set environment variables when running a Python program.
+    Both from the IDE (PyCharm) or from the command line.
 """
 import os
 
